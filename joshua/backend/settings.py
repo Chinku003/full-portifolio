811063b9a13fd1cf9236d235dd04a278715e602a
ModernaCyber <modernacyber@gmail.com>
2026-08-13 11:54:58 +0300

    changes for the dotenv module

--- joshua/backend/settings.py
+++ joshua/backend/settings.py
@@ -13,14 +13,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
+try:
+    from dotenv import load_dotenv
+except ImportError:
+    load_dotenv = None
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / '.env')
-
-
+if load_dotenv:
+    load_dotenv(BASE_DIR / ".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
